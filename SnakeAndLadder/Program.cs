7938e5c8e4ed1240a39e4e12561ea2d842a8b796
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-04 17:46:04 +0530

    feat: Added : Game Options - No play, Ladder or Snake

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -9,13 +9,36 @@ namespace SnakeAndLadder
             //Display Welcome Message
             Console.WriteLine("Welcome to Sanke & Ladder Simulator");
 
-            //Variable
+            //Variables
             int start_point = 0;
+            int position = 0;
+
+            //Generate Numbers For Die and GameOptions
             Random rand = new Random();
             int dieNumber = rand.Next(1,7);
+            int gameOptions = rand.Next(0, 3);
 
             //Display Die Number
             Console.WriteLine("Die Number : " +dieNumber);
+
+            switch (gameOptions)
+            {
+                case 0:
+                    Console.WriteLine("Position : " + position);
+                    break;
+                case 1:
+                    position += dieNumber;
+                    Console.WriteLine("Position : " + position);
+                    break;
+
+                case 2:
+                    position -= dieNumber;
+                    Console.WriteLine("Position : " + position);
+                    break;
+                
+                default:                    
+                    break;
+            }
         }
     }
 }
